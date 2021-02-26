f938907b811dc84f4b6ed5321aabb5cba7c5bf6b
PTSGFX <76948129+PTSGFX@users.noreply.github.com>
2021-02-26 17:46:15 +0545

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,11 +9,17 @@ $subject = S_REQUEST['Message'];
 if (empty($fname) || empty($lname) || empty($subject)) {
 echo('Please fill all the fields');
 }else {
-  mail("piyushtechs1@gmail.com", "Webtech Message", $fname, "From: $lname <$subject>");
-echo "<script type='text/javascript'>alert( 'your message has been sent successfully');
-window.history.log(-1)
-</script>";
-}
+  $to ="traidevindia@gmail.com"; 	 
+  $headers = "Form : $fname \r\n";	
+  $headers .= "Reply-To : $lname \r\n"; 	
+  $run = mail($to,$fname,$lname,$subject,$headers); 
+  if($run)	 
+    echo "<script>alert('Form Submitted!');
+    location.href='index.html';
+    </script>";	 
+
+
+
 
 ?>
 
